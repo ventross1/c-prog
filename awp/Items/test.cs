f9db51726f1bacb31d4fa8aa959f6e9ceaaa3ae4
vašek chvátal <38693443+ventross1@users.noreply.github.com>
2022-04-04 23:18:46 +0200

    Add files via upload
    test

---
+++ awp/Items/test.cs
@@ -0,0 +1,42 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace awp.Items
+{
+	public class Awp : ModItem
+	{
+		public override void SetStaticDefaults() 
+		{
+			// DisplayName.SetDefault("test"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
+			Tooltip.SetDefault("This is a basic modded sniper rifle");
+		}
+
+		public override void SetDefaults() 
+		{
+			item.damage = 50;
+			item.melee = true;
+			item.width = 40;
+			item.height = 40;
+			item.useTime = 20;
+			item.useAnimation = 20;
+			item.useStyle = 1;
+			item.knockBack = 6;
+			item.value = 10000;
+			item.rare = 2;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
+		}
+
+		public override void AddRecipes() 
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.IllegalGunParts);
+			recipe.AddIngredient(ItemID.SniperRifle);
+			recipe.AddIngredient(ItemID.SniperScope);
+			recipe.AddIngredient(ItemID.SoulofFright, 100);
+			recipe.AddTile(TileID.MythrilAnvil);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
---
+++ awp/awp.cs
@@ -0,0 +1,8 @@
+using Terraria.ModLoader;
+
+namespace awp
+{
+	public class awp : Mod
+	{
+	}
+}
